aadc9cef5ec9893cb9aba1dedbd7c1a3e3be261d
nslcoder <lokeshgurungwj@gmail.com>
2024-11-02 09:57:19 +0545

    Solve Pete, the baker

---
+++ codewars/5kyu/pete-the-baker/solution.js
@@ -0,0 +1,24 @@
+/**
+ * Title: Pete, the baker
+ * 
+ * Rank: 5 kyu
+ * 
+ * Description: Pete likes to bake some cakes. He has some recipes and ingredients. Unfortunately he is not good in maths. Can you help him to find out, how many cakes he could bake considering his recipes?
+
+ Write a function cakes(), which takes the recipe (object) and the available ingredients (also an object) and returns the maximum number of cakes Pete can bake (integer). For simplicity there are no units for the amounts (e.g. 1 lb of flour or 200 g of sugar are simply 1 or 200). Ingredients that are not present in the objects, can be considered as 0.
+ * 
+ * Link: https://www.codewars.com/kata/525c65e51bf619685c000059
+ */
+
+function cakes(recipe, available) {
+  const recipeKeys = Object.keys(recipe);
+  const count = {};
+
+  recipeKeys.forEach((key) => {
+    count[key] = available[key] ? Math.floor(available[key] / recipe[key]) : 0;
+  });
+
+  const maxCakes = Math.min(...Object.values(count));
+
+  return maxCakes;
+}
